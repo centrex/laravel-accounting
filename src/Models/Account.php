ac438f3cedb6d2f38f2babe757a7704621724b9f
rochi88 <raisul.me@gmail.com>
2024-09-23 19:06:30 +0600

    fix connection error

--- src/Models/Account.php
+++ src/Models/Account.php
@@ -12,4 +12,17 @@ class Account extends Model
     use HasAccountingJournal;
 
     protected $guarded = [];
+
+    /** @var string */
+    protected $table = 'accounts';
+
+    /**
+     * Specify the connection, since this implements multitenant solution
+     * Called via constructor to faciliate testing
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setConnection(config('accounting.drivers.database.connection'));
+    }
 }
