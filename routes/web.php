05be8234971dbf15732b78571fc6c849d8d8aadc
rochi88 <raisul.me@gmail.com>
2025-11-29 13:12:58 +0600

    update route prefix

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 use Centrex\LaravelAccounting\Livewire\{AccountingDashboard, ChartOfAccounts, FinancialReports, JournalEntries};
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->prefix('acct')->group(function () {
+Route::middleware(['auth'])->prefix('accounting')->group(function () {
     Route::get('/dashboard', AccountingDashboard::class)->name('accounting.dashboard');
     Route::get('/accounts', ChartOfAccounts::class)->name('accounting.accounts');
     Route::get('/journal-entries', JournalEntries::class)->name('accounting.journal');
